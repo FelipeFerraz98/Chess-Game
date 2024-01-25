bbb6f68d8166023e53b8b54f7eeaa17710725a75
Felipe Ferraz <felipeferraz1398@gmail.com>
2024-01-25 19:16:40 -0300

    Insert ChessPosition

---
+++ Chess/folderchess/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿
+
+using folderboard;
+
+namespace folderchess
+{
+    class ChessPosition
+    {
+        public char column { get; set; }
+        public int row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+    }
+}
